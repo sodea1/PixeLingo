3abe2a9b299aeb7469780b344733817aa63fb37d
seanodea <seanodea11@gmail.com>
2022-07-13 22:04:35 -0400

    store setup complete with configureStore instead of deprecated createStore

--- frontend/pixelingo.jsx
+++ frontend/pixelingo.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { createRoot } from "react-dom";
-import { setupStore } from "./store/store";
+import { store } from "./store/store";
+import * as ReactDOMClient from 'react-dom/client';
+import Test from "./components/test";
 
 document.addEventListener("DOMContentLoaded", () => {
-    const root = document.getElementById("root");
-    // v18-
-    // ReactDOM.render(<h1>Welcome homedog</h1>, root)
-    // v18+
-    const store = setupStore;
-    window.getState = store.getState();
-    window.dispatch = store.dispatch();
-    const reactRoot = createRoot(root);
-    reactRoot.render(<h1>Welcome homedog</h1>);
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
+
+    const container = document.getElementById("root");
+    const root = ReactDOMClient.createRoot(container);
+    root.render(<Test store={store} />);
 })
--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,8 +1,13 @@
 import { createStore } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from '../reducers/root_reducer';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
-export const setupStore = configureStore({
-    reducer: rootReducer
-})
+const reducer = rootReducer;
+
+export const store = configureStore({   
+    reducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger).concat(thunk)
+});
 
