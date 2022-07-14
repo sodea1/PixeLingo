12f0786be43c017977aed015ae42c64efcc8f5d3
seanodea <seanodea11@gmail.com>
2022-07-14 10:06:42 -0400

    photo api cleaup

--- frontend/util/photo_api_util.js
+++ frontend/util/photo_api_util.js
@@ -3,8 +3,4 @@ const axios = require("axios").default;
 export const getImage = () => axios({
     method: "GET",
     url: `https://api.unsplash.com/photos/random/?client_id=${imageKey}&orientation=landscape`,
-    // orientation: "landscape",
-    // auto
-    // w: 700,
-    // h: 400,
 }).then(res => res.data.urls.raw);
