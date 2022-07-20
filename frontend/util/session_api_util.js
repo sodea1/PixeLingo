b25c474fc1d9d4e6fba41883f1da0ec0295c1b2d
seanodea <seanodea11@gmail.com>
2022-07-19 21:35:42 -0400

    CSRF token issue

---
+++ frontend/util/session_api_util.js
@@ -0,0 +1,17 @@
+import axios from "axios"
+
+export const login = (user) => {
+    return axios({
+        method: 'POST',
+        url: '/api/session',
+        data: { user }
+    });
+};
+
+export const signup = (user) => {
+    return axios({
+        method: "POST",
+        url: '/api/users',
+        data: { user }
+    })
+}
