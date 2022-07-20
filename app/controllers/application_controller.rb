cbd3eb60c927f50db7b58210762c2d1035796769
seanodea <seanodea11@gmail.com>
2022-07-19 20:58:48 -0400

    session controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,19 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user, :logged_in?
     # CL(CUBED)R
     def current_user
         @current_user ||= User.find_by(session_token: session[:session_token])
     end
 
-    def login
-
+    def login(user)
+        @current_user = user
+        session[:session_token] = user.reset_session_token!
     end
 
     def logout
+        current_user.reset_session_token!
+        session[:session_token] = nil
+        current_user = nil
 
     end
 
