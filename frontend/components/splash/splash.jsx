0dcf9fc86b684e8acb8ad4728b325aed41bd29df
seanodea <seanodea11@gmail.com>
2022-07-26 16:16:51 -0400

    multiple pics moving on top of each other

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -7,13 +7,14 @@ const Splash = () => {
     const [isLoaded, setIsLoaded] = useState(false);
     const [images, setImages] = useState([]);
     const [error, setError] = useState(null);
+    const [image, setImage] = useState(false)
 
     // useEffect(() => {
     //     const topic = Helpers.getTopic();
     //     getCollection(topic)
     //         .then((res) => res.data)
     //         .then((res) => {
-    //             const urls = res.map(obj => obj.urls.small)
+    //             const urls = res.map(obj => obj.urls.thumb)
     //             setImages(urls)
     //             setIsLoaded(true)
     //         }, (error) =>{
@@ -21,32 +22,50 @@ const Splash = () => {
     //         });
     // }, []);
 
+    useEffect(() => {
+        setImage(true)
+    }, [])
+
     const animate = () => {
-        setInterval(() => {
-            const image = document.getElementById("test1");
-            let height = image.offsetTop;
-            const maxHeight = window.innerHeight;
-            // let w = image.offsetLeft;
-            // const maxWidth = window.innerWidth;
-            let newHeight = height + 2;
-            image.style.top = String(newHeight) + "px";
-        }, 10)
-    }
+        const allImages = document.getElementsByClassName("test1");
+        for (let image of allImages) {
+            setInterval(() => {
+                let height = image.offsetTop;
+                const maxHeight = window.innerHeight;
+                let newHeight = height + 1;
+                if (newHeight === maxHeight) {
+                    image.style.top = String(-(image.naturalHeight)) + "px";
+                } else {
+                    image.style.top = String(newHeight) + "px";
+                }
+            }, 10)
+        };
+    };
 
     useEffect(() => {
         animate()
-    }, [isLoaded])
+    }, [image])
     
     return (
         <div className='splash-container'>
-            <img id='test1' src='https://images.unsplash.com/photo-1417325384643-aac51acc9e5d?q=75&fm=jpg&w=200&fit=max' />
-            <img id='test2' src='https://images.unsplash.com/photo-1417325384643-aac51acc9e5d?q=75&fm=jpg&w=200&fit=max' />
+           
             <div className='title'>PixeLingo</div>
+            <div className='waterfall-wrapper'>
+                <img className='test1' src='https://images.unsplash.com/photo-1417325384643-aac51acc9e5d?q=75&fm=jpg&w=200&fit=max' />
+                <img className='test1' src='https://images.unsplash.com/photo-1417325384643-aac51acc9e5d?q=75&fm=jpg&w=200&fit=max' />
+                <img className='test1' src='https://images.unsplash.com/photo-1417325384643-aac51acc9e5d?q=75&fm=jpg&w=200&fit=max' />
+                <img className='test1' src='https://images.unsplash.com/photo-1417325384643-aac51acc9e5d?q=75&fm=jpg&w=200&fit=max' />
+                <img className='test1' src='https://images.unsplash.com/photo-1417325384643-aac51acc9e5d?q=75&fm=jpg&w=200&fit=max' />
+                <img className='test1' src='https://images.unsplash.com/photo-1417325384643-aac51acc9e5d?q=75&fm=jpg&w=200&fit=max' />
+                <img className='test1' src='https://images.unsplash.com/photo-1417325384643-aac51acc9e5d?q=75&fm=jpg&w=200&fit=max' />
+                <img className='test1' src='https://images.unsplash.com/photo-1417325384643-aac51acc9e5d?q=75&fm=jpg&w=200&fit=max' />
                 {/* {isLoaded && images.map((url, i) => {
+                    animate(<img className='animate' id={"pic" + i} src={url} key={i} />)
                     return (
-                        <img src="" key={i} />
+                        <img className='animate' id={"pic" + i} src={url} key={i} />
                     )
                 })} */}
+            </div>
         </div>
     )
 };
--- frontend/util/image_api_util.js
+++ frontend/util/image_api_util.js
@@ -13,7 +13,7 @@ export const getImage = (topic) => {
 export const getCollection = (topic, count = 30) => {
     return axios({
         method: "GET",
-        url: `https://api.unsplash.com/photos/random/?client_id=${imageKey}&orientation=squarish`,
+        url: `https://api.unsplash.com/photos/random/?client_id=${imageKey}`,
         params: {
             query: topic,
             count: count
