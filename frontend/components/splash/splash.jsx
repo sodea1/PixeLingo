fa34b734f12e83bbcfb41f0eaf31663f77da2093
seanodea <seanodea11@gmail.com>
2022-07-26 21:08:04 -0400

    splash formatting; image shuffle

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -5,68 +5,55 @@ import * as Helpers from './splash_helpers';
 
 const Splash = () => {
     const [isLoaded, setIsLoaded] = useState(false);
-    const [images, setImages] = useState([]);
+    const [urls, setUrls] = useState([]);
     const [error, setError] = useState(null);
-    const [image, setImage] = useState(false)
 
     useEffect(() => {
         const topic = Helpers.getTopic();
         getCollection(topic)
+            .then((res) => res.data)
             .then((res) => {
-                debugger
-                res.data
-            })
-            .then((res) => {
-                const urls = res.map(obj => obj.urls.thumb)
-                setImages(urls)
+                const urls = res.map(obj => obj.urls.raw);
+                setUrls(urls)
                 setIsLoaded(true)
             }, (error) =>{
                 setError(error)
             });
     }, []);
 
-    useEffect(() => {
-        setImage(true)
-    }, [])
+    const slideshow = (urls) => {
+        let idx = 0;
+        setInterval(() => {
+            const image = document.getElementById("splash-image");
+            if (idx === urls.length - 1) idx = 0;
+            image.src = urls[idx];
+            idx++;
+        }, 5000)
+    }
 
-    const animate = () => {
-        const allImages = document.getElementsByClassName("test1");
-        for (let image of allImages) {
-            setInterval(() => {
-                let height = image.offsetTop;
-                const maxHeight = window.innerHeight;
-                let newHeight = height + 1;
-                if (newHeight === maxHeight) {
-                    image.style.top = String(-(image.naturalHeight)) + "px";
-                } else {
-                    image.style.top = String(newHeight) + "px";
-                }
-            }, 20)
-        };
-    };
-
-    const placeImages = (num) => {
-        const maxWidth = window.innerWidth;
-        const numPics = Math.floor(maxWidth / 210);
-        const startX = 4;
-
-        let xPos;
-        if (num === 0) {
-
-        }
-    };
+    // const animate = () => {
+    //     const allImages = document.getElementsByClassName("test1");
+    //     for (let image of allImages) {
+    //         setInterval(() => {
+    //             let height = image.offsetTop;
+    //             const maxHeight = window.innerHeight;
+    //             let newHeight = height + 1;
+    //             if (newHeight === maxHeight) {
+    //                 image.style.top = String(-(image.naturalHeight)) + "px";
+    //             } else {
+    //                 image.style.top = String(newHeight) + "px";
+    //             }
+    //         }, 20)
+    //     };
+    // };
     
     return (
         <div className='splash-container'>
            
             <div className='title'>PixeLingo</div>
             {/* <div className='waterfall-wrapper'> */}
-            <div className=''>
-                {isLoaded && images.map((url, i) => {
-                    return <img className='test1' id={"pic" + i} src={url} key={i} />
-                    // return startCoords(num);
-                })}
-            </div>
+            <img id='splash-image' src="" width="400px" />
+            {urls.length > 0 && slideshow(urls)}
              
         </div>
     )
